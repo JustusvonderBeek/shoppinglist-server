062e0865a53e53c167e4142b578dbe94642bc2e2
Justus <beek@in.tum.de>
2023-12-16 19:52:15 +0100

    Allow get & post items and users

--- server/server.go
+++ server/server.go
@@ -2,29 +2,66 @@ package server
 
 import (
 	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"shop.cloudsheeptech.com/configuration"
+	"shop.cloudsheeptech.com/database"
 )
 
-func getShopList(c *gin.Context) {
+func getAllItems(c *gin.Context) {
+	log.Printf("Trying to access all items")
+	items, err := database.GetAllItems()
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, items)
+	log.Printf("Send %d items back to requester", len(items))
+}
+
+func getItem(c *gin.Context) {
 	sId := c.Param("id")
 	id, err := strconv.Atoi(sId)
 	if err != nil {
-		log.Printf("Failed to parse given list id: %s", sId)
+		log.Printf("Failed to parse given item id: %s", sId)
 		log.Printf("Err: %s", err)
 		return
 	}
-	log.Printf("Trying to access list: %d", id)
+	log.Printf("Trying to access item: %d", id)
+	item, err := database.GetItem(id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, item)
+	log.Print("Send item back to requester")
+}
 
+func addItem(c *gin.Context) {
+	var item database.Item
+	if err := c.BindJSON(&item); err != nil {
+		log.Printf("Item is in incorrect format: %v", c.Request.Body)
+		return
+	}
+	id, err := database.InsertItem(item)
+	if err != nil || id < 0 {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.IndentedJSON(http.StatusAccepted, id)
+	log.Printf("Inserted item under ID %d", id)
 }
 
 func Start(cfg configuration.Config) error {
 	gin.SetMode(gin.DebugMode)
 
 	router := gin.Default()
-	router.GET("/list/:id", getShopList)
+	router.GET("/items", getAllItems)
+	router.GET("/items/:id", getItem)
+
+	router.POST("/items", addItem)
 
 	address := cfg.ListenAddr + ":" + cfg.ListenPort
 	router.Run(address)
