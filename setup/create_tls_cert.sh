d335cd40cd84cfa3455638d19860ab6d7257fb85
Justus von der Beek <justusvonderbeek@gmail.com>
2023-12-25 18:39:38 +0100

    Added script to create tls certificate

---
+++ setup/create_tls_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+outputPath="../resources/"
+outputCert="shoppinglist.crt"
+outputKey="shoppinglist.pem"
+
+echo "Creating new certificate and keyfile."
+
+openssl req -x509 -newkey rsa:4096 -keyout "$outputPath$outputKey" -out "$outputPath$outputCert" -sha512 -days 365 -nodes -subj "/C=DE/ST=Bavaria/L=Munich/O=Cloudsheeptech/OU=Shoppinglist/CN=shop.cloudsheeptech.com"
+
+echo "Certificate and keyfile successfully created!"
