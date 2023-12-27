fd5cc135a588da9417b323923bf3a1194cb669e7
Justus <beek@in.tum.de>
2023-12-27 20:59:35 +0100

    Added hint for DB password

--- setup/setup_server.sh
+++ setup/setup_server.sh
@@ -7,6 +7,10 @@ echo "Creating TLS Certificates and setup database"
 # Create TLS Certificates
 ./create_tls_cert.sh
 
+echo "Is the password for the database already correctly inserted into the SQL file?"
+
+read -p "Press enter to continue"
+
 # Create the tables in the database
 sudo mysql < ./create_mysql_db.sql
 
