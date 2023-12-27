110a5cb4ed1be286d2699e893dc3327f5dec1248
Justus <beek@in.tum.de>
2023-12-27 20:59:20 +0100

    Fixed not creating resource dir

--- setup/create_tls_cert.sh
+++ setup/create_tls_cert.sh
@@ -6,6 +6,9 @@ outputKey="shoppinglist.pem"
 
 echo "Creating new certificate and keyfile."
 
+# Creating the resource folder if it does not exists yet
+mkdir -p $outputPath
+
 openssl req -x509 -newkey rsa:4096 -keyout "$outputPath$outputKey" -out "$outputPath$outputCert" -sha512 -days 365 -nodes -subj "/C=DE/ST=Bavaria/L=Munich/O=Cloudsheeptech/OU=Shoppinglist/CN=shop.cloudsheeptech.com"
 
 echo "Certificate and keyfile successfully created!"
