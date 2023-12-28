1107a74e3af34aa6ef0c9e94c3f6b6094a97fb3f
Justus <beek@in.tum.de>
2023-12-28 14:15:48 +0100

    Updated db config file location

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"log"
 	"os"
 
-	"shop.cloudsheeptech.com/configuration"
 	"shop.cloudsheeptech.com/database"
 	"shop.cloudsheeptech.com/server"
+	"shop.cloudsheeptech.com/server/configuration"
 )
 
 func main() {
@@ -16,7 +16,7 @@ func main() {
 	addr := flag.String("a", "0.0.0.0", "Listen address")
 	port := flag.String("p", "46152", "Listen port")
 	logfile := flag.String("l", "server.log", "The logfile location")
-	dbConfig := flag.String("c", "db.json", "The database configuration file")
+	dbConfig := flag.String("c", "resources/db.json", "The database configuration file")
 	tlscert := flag.String("cert", "resources/shoppinglist.crt", "The location of the TLS Certificate")
 	tlskey := flag.String("key", "resources/shoppinglist.pem", "THe location of the TLS keyfile")
 	jwtFile := flag.String("jwt", "resources/jwt.secret", "The path to the file holding the JWT Secret")
