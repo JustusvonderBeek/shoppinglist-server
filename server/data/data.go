d822fcec3a913366b4972d0ba41da07453ecd0bd
Justus <beek@in.tum.de>
2023-12-28 14:16:49 +0100

    Started separate data package

---
+++ server/data/data.go
@@ -0,0 +1,14 @@
+package data
+
+// Helping file containing all data structures
+
+// ------------------------------------------------------------
+// The data for authentication and login
+// ------------------------------------------------------------
+
+type User struct {
+	ID       int64
+	Username string
+	Passwd   string
+	Salt     string
+}
