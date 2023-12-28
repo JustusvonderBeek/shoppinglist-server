5e198e394042fa9840f26f4cc9372ace5d3e0135
Justus <beek@in.tum.de>
2023-12-28 14:59:19 +0100

    Added modify user tests

--- database/database_test.go
+++ database/database_test.go
@@ -136,6 +136,96 @@ func TestUserLogin(t *testing.T) {
 	database.ResetUserTable()
 }
 
+func TestModifyUsername(t *testing.T) {
+	connectDatabase()
+	user := data.User{
+		ID:       12,
+		Username: "Test User modify",
+		Passwd:   "Biene Maja",
+	}
+	createdUser, err := database.CreateUserAccount(user.Username, user.Passwd)
+	if err != nil {
+		log.Printf("Failed to insert user into database: %s", err)
+		t.FailNow()
+	}
+	checkOldUsername, err := database.GetUser(createdUser.ID)
+	if err != nil {
+		log.Printf("Failed to get newly created user for modify check: %s", err)
+		t.FailNow()
+	}
+	if checkOldUsername.Username != user.Username {
+		log.Print("Usernames do not match before checking!")
+		t.FailNow()
+	}
+	updatedUsername, err := database.ModifyUserAccountName(createdUser.ID, user.Username+" - Updated")
+	if err != nil {
+		log.Printf("Failed to update username: %s", err)
+		t.FailNow()
+	}
+	database.PrintUserTable("shoppers")
+	if updatedUsername.Username == checkOldUsername.Username {
+		log.Print("The updated username is still the same!")
+		t.FailNow()
+	}
+	checkNewUsername, err := database.GetUser(createdUser.ID)
+	if err != nil {
+		log.Printf("Failed to get updated user: %s", err)
+		t.FailNow()
+	}
+	if checkNewUsername.Username != user.Username+" - Updated" {
+		log.Print("Usernames do not match!")
+		t.FailNow()
+	}
+	log.Print("TestModifyUsername successfully completed")
+	database.ResetUserTable()
+}
+
+func TestModifyUserPassword(t *testing.T) {
+	connectDatabase()
+	user := data.User{
+		ID:       12,
+		Username: "Test User modify",
+		Passwd:   "Old Password",
+	}
+	createdUser, err := database.CreateUserAccount(user.Username, user.Passwd)
+	if err != nil {
+		log.Printf("Failed to insert user into database: %s", err)
+		t.FailNow()
+	}
+	checkOldPassword, err := database.GetUser(createdUser.ID)
+	if err != nil {
+		log.Printf("Failed to get newly created user for modify check: %s", err)
+		t.FailNow()
+	}
+	if checkOldPassword.Passwd != createdUser.Passwd {
+		log.Print("Password do not match before update!")
+		t.FailNow()
+	}
+	updatedUser, err := database.ModifyUserAccountPassword(createdUser.ID, "New Password")
+	if err != nil {
+		log.Printf("Failed to update password: %s", err)
+		t.FailNow()
+	}
+	database.PrintUserTable("shoppers")
+	match, err := argon2id.ComparePasswordAndHash("New Password", updatedUser.Passwd)
+	if err != nil || !match {
+		log.Print("The password was not correctly updated!")
+		t.FailNow()
+	}
+	checkNewPassword, err := database.GetUser(createdUser.ID)
+	if err != nil {
+		log.Printf("Failed to get updated user: %s", err)
+		t.FailNow()
+	}
+	match, err = argon2id.ComparePasswordAndHash("New Password", checkNewPassword.Passwd)
+	if err != nil || !match {
+		log.Print("The password was not correctly updated!")
+		t.FailNow()
+	}
+	log.Print("TestModifyUserPassword successfully completed")
+	database.ResetUserTable()
+}
+
 // ------------------------------------------------------------
 // Testing data handling
 // ------------------------------------------------------------
