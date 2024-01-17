b629fe126f78415e202ca5f01914a81079c2136d
Justus <beek@in.tum.de>
2024-01-17 18:30:08 +0100

    Updated jwt creation script

--- setup/create_jwt_secret.sh
+++ setup/create_jwt_secret.sh
@@ -2,6 +2,12 @@
 
 echo "Creating new JWT secret"
 
-tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 32; echo
+secret=$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 32; echo)
+echo "Secret: $secret"
+
+echo """{
+	\"Secret\": \"$secret\",
+	\"ValidUntil\": \"2024-01-01T15:00:00+01:00\"
+}""" > ../resources/jwtSecret.json
 
 echo "Secret created"
