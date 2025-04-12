c01d66ac08c960b046be11f35a08f21f3601e056
Justus von der Beek <justusvonderbeek@gmail.com>
2025-04-12 11:40:53 +0200

    feat: create signing config parameter files

--- setup/create_tls_cert.sh
+++ setup/create_tls_cert.sh
@@ -6,6 +6,9 @@ if [[ $# -lt 1 ]]; then
 fi
 outputCert="shoppinglist.crt"
 outputKey="shoppinglist.pem"
+
+serverSign="servercert.csr"
+serverCert=""
 serverConfig="server-cert.conf"
 rootConfig="root-ca.conf"
 
@@ -14,12 +17,16 @@ echo "Creating new certificate and keyfile."
 # Creating the resource folder if it does not exists yet
 mkdir -p $outputPath
 
-openssl req -config "$serverConfig" -newkey rsa:4096 -sha256 -nodes -out servercert.csr -outform PEM
+openssl req -config "$serverConfig" -newkey rsa:4096 -sha256 -nodes -out "$serverSign" -outform PEM
 
-touch index.txt
-echo '01' > serial.txt
+if [[ ! -f index.txt ]]; then
+  touch index.txt
+fi
+if [[ ! -f serial.txt ]]; then
+  echo '01' > serial.txt
+fi
 
-openssl ca -config "$rootConfig" -policy signing_policy -extensions signing_req -out servercert.pem -infiles servercert.csr
+openssl ca -config "$rootConfig" -policy signing_policy -extensions signing_req -out servercert.pem -infiles "$serverSign"
 
 #openssl req -x509 -newkey rsa:4096 -keyout "$outputPath$outputKey" -out "$outputPath$outputCert" -sha512 -days 365 -nodes -subj "/C=DE/ST=Bavaria/L=Munich/O=Cloudsheeptech/OU=Shoppinglist/CN=shop.cloudsheeptech.com"
 
