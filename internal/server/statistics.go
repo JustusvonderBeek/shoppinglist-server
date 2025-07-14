3e814c5699d17b0a863608a389daf4e7f51d890c
Justus von der Beek <justusvonderbeek@gmail.com>
2025-07-14 20:10:36 +0200

    feat: move statistics into own file

---
+++ internal/server/statistics.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// ------------------------------------------------------------
+// Monitoring functionality
+// ------------------------------------------------------------
+
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"path"},
+	)
+
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"path"},
+	)
+
+	activeConnections = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "active_connections",
+			Help: "Number of active connections",
+		},
+	)
+)
+
+func SetupMonitoring() {
+	prometheus.MustRegister(httpRequestsTotal, httpRequestDuration, activeConnections)
+}
+
+// Middleware to track Prometheus metrics
+func prometheusMiddleware(c *gin.Context) {
+	path := c.Request.URL.Path
+
+	// begin timer to measure the requests duration
+	timer := prometheus.NewTimer(httpRequestDuration.WithLabelValues(path))
+
+	// increment total request counter
+	httpRequestsTotal.WithLabelValues(path).Inc()
+
+	// increment number of active connections
+	activeConnections.Inc()
+
+	// complete processing request
+	c.Next()
+
+	// record request duration (post processing)
+	timer.ObserveDuration()
+
+	// decrement total number of active connections (post processing)
+	activeConnections.Dec()
+}
